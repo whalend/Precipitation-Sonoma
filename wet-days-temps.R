2a9e764a10335bd36ddba01816f0aa28a46e5226
Whalen <whalendillon@gmail.com>
2016-02-05 22:59:49 -0500

    Add script developing wet season temperature variables.

---
+++ wet-days-temps.R
@@ -0,0 +1,9 @@
+#' # Wet Days Temperature Variables
+#' 
+#' Calculate the number of hours within selected temperature ranges during days that had measurable rainfall for each season.
+#' 
+#+ load data ####
+## data developed in 'assign-plots-rgs.R' script
+wet_days <- read.csv("wet-days-temps.csv")
+
+
